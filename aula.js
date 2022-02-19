418e68e3fe1b76af544197b69931883244ee6f34
Eric Sampaio <erick.gtn.xp@gmail.com>
2022-02-18 21:12:21 -0300

    comandos iniciais do JS
    Comandos iniciais do JS

---
+++ aula.js
@@ -0,0 +1,23 @@
+//Variavel
+let idade = 20
+
+//constante
+const valorIngresso = 30
+
+
+//Tipos Primitivos
+let nome = 'Eric';//string
+let numero = 25; // numero literal
+let estatusAprovacao = true; // boleano
+let sobrenome; // undefined
+let corSelecionado = null; // Redefinir um valor
+
+//Objeto
+let pessoa = {
+    nome: 'Eric',
+    idade: 25,
+    cidade:"Camaçari"
+};
+console.log(pessoa.cidade)
+
+//Arrays
